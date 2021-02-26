93bdf55f92286b5fc4a3d69d67ca207ad8d252b1
barretopcarlos <barretopcarlos@gmail.com>
2021-02-26 18:35:05 -0300

    criando js

---
+++ snake/scripts.js
@@ -0,0 +1,12 @@
+let canvas = documento.getElementById("snake");
+let contexto = canvas.getContext("2d");
+let box = 32;
+
+function criarBG(){
+    contexto.fillStyle = "lightgreen";
+    contexto.fillRect(0, 0, 16*box , 16*box);
+}
+
+criarBG();
+
+ 
