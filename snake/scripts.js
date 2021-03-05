162a18b16a80f8c6b5d7d9271a9c2bcbba981bc4
barretopcarlos <barretopcarlos@gmail.com>
2021-03-05 17:34:43 -0300

    Jogo finalizado

--- snake/scripts.js
+++ snake/scripts.js
@@ -1,87 +1,84 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext("2d");
+ 
+let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
+let context = canvas.getContext("2d"); //....
 let box = 32;
-let snake = [];
-
-
-snake[0] = {
+let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
+snake[0] ={
     x: 8 * box,
     y: 8 * box
 }
-
-let direcao = "right";
-
-let food = {
+let direction = "right";
+let food ={
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box , 16 * box);
+    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
-
-
-function criarCobra(){
-
-    for(i=0; i < snake.length; i++){
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y , box , box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
-
 }
 
-function drawFood(){
-
+function drawFood (){
     context.fillStyle = "red";
-    context.fillRect(food.x,food.y,box,box);
-
+    context.fillRect(food.x, food.y, box, box);
 }
 
-//pegando as teclas digitadas
+//quando um evento acontece, detecta e chama uma função
 document.addEventListener('keydown', update);
 
 function update(event){
-    if(event.keyCode == 37 && direcao != "right") direcao = "left";
-    if(event.keyCode == 38 && direcao != "down") direcao = "up";
-    if(event.keyCode == 39 && direcao != "left") direcao = "right";
-    if(event.keyCode == 40 && direcao != "up") direcao = "down";
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function iniciarJogo(){
+function iniciarJogo(){    
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
     
-    if(snake[0].x > 15 * box && direcao == "right") snake[0].x = 0;
-    if(snake[0].x < 0 * box && direcao == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direcao == "down") snake[0].y = 0;
-    if(snake[0].y < 0 * box && direcao == "up") snake[0].y = 16 * box;
-    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Perdeu!');
+        }
+    }
+
     criarBG();
-    criarCobra();
+    criarCobrinha();
     drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direcao == "right") snakeX += box;
-    if(direcao == "left") snakeX -= box;
-    if(direcao == "up") snakeY -= box;
-    if(direcao == "down") snakeY += box;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    snake.pop();
-
-    let newHead = {
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
+    let newHead ={
         x: snakeX,
         y: snakeY
     }
 
-    snake.unshift(newHead);
-
+    snake.unshift(newHead); //método unshift adiciona como primeiro quadradinho da cobrinha
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
-
-
-
- 
